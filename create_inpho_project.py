e169fb7c137d3e65f19d97ebc201065554728345
AnnPla <anna.platek2222@gmail.com>
2021-06-08 16:32:08 +0200

    search some photo and camera parameters

--- create_inpho_project.py
+++ create_inpho_project.py
@@ -3,10 +3,15 @@
 
 import os
 import argparse
+#from inpho_pix_methods import search_coor_system
+#from inpho_pix_methods import search_navigation_params
 import pandas as pd
 
 parser = argparse.ArgumentParser()
 args, unknown = parser.parse_known_args()
+
+params_pix_path = 'D:\\PW\\inpho_pix\\Malbork_AT\\pix\\Malbork_pix\\1_initial\\params\\'
+photos_path = 'D:\\AP\\inpho\\Malbork_AT\\Zdjecia\\'
 
 
 
@@ -26,7 +31,9 @@ sds_obj_gr_xy = ['-1.000000', '-1.000000', '-1.000000', '-1.000000']
 sds_obj_gr_z = ['-1.000000', '-1.000000', '-1.000000', '-1.000000']
 refract_cord_default = 'on'
 curv_cord_default = 'on'
-coord_sys = ''
+
+
+
 lin_units_obj = 'm'
 lin_units_img = 'mm'
 ang_units = 'deg'
@@ -35,20 +42,9 @@ gps_std = ['0.100000', '0.100000', '0.250000']
 ins_std = ['0.100000', '0.100000', '0.500000']
 photos = 'photos'
 
-camera_type = 'hasselbladt'
-camera_date = '14:16:07 25/04/2017'
-camera_brand = 'Custom'
-camera_kind = 'CCDFrame'
-ccd_in_orient = ['-0.0000000000', '-166.6666666667', '4102.1667000000', '-166.6666666667', '-0.0000000000', '3052.7167000000']
-ccd_col = '8176'
-ccd_rows = '6132'
-pxl_ref = 'CenterTopLeft'
-focal_len = '50.175400'
-prin_point_PPA = ['0.000000', '0.000000']
-gps_antenna_offset = ['0.000000', '0.000000', '0.000000']
-camera_mount_rotation = '0.000000'
 
-navigation = ''
+
+#navigation = search_navigation_params(params_pix_path)
 
 
 with open(f'{inpho_prj_path}{inpho_file_name}.prj', 'w+', encoding='utf-8') as f:
@@ -68,8 +64,16 @@ with open(f'{inpho_prj_path}{inpho_file_name}.prj', 'w+', encoding='utf-8') as f
     f.write(f'  $SDS_OBJ_GROUP_Z :  {sds_obj_gr_z[0]} {sds_obj_gr_z[1]} {sds_obj_gr_z[2]} {sds_obj_gr_z[3]}\n')
     f.write(f'  $REFRACT_CORR_DEFAULT : {refract_cord_default}\n')
     f.write(f'  $CURV_CORR_DEFAULT : {curv_cord_default}\n')
+
     f.write(f'  $COORDINATE_SYSTEM :\n')
-    f.write(f'{coord_sys}\n')
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_wkt.prj'):
+                with open(f'{params_pix_path}{file}', 'r') as coo_sys_f:
+                    for line in coo_sys_f:
+                        coord_sys = line
+    f.write(f'     {coord_sys}\n')
+
     f.write(f'  $LINEAR_UNITS_OF_OBJECT : {lin_units_obj}\n')
     f.write(f'  $LINEAR_UNITS_OF_IMAGE : {lin_units_img}\n')
     f.write(f'  $ANGULAR_UNITS : {ang_units}\n')
@@ -83,34 +87,191 @@ with open(f'{inpho_prj_path}{inpho_file_name}.prj', 'w+', encoding='utf-8') as f
     f.write(f'$END\n')
 
     #PHOTO
-    f.write(f'{photos}\n')
+    #photo list
+    photo_lst = list()
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_calibrated_external_camera_parameters_error.txt'):
+                df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+
+    for index, rows in df.iterrows():
+        photo_lst.append(rows.imageName)
+
+    #print(photo_lst)
+
+    #points list
+    points_list = list()
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_measured_estimated_gcps_position.txt'):
+                df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+
+    for index, rows in df.iterrows():
+        points_list.append(rows['GCP label'])
+
+
+    #list of measured points on photo
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_tp_orima.txt'):
+                points_file = file
+
+    with open(f'{params_pix_path}{points_file}', 'r') as points_f:
+        measured_point_lst = list()
+        for line in points_f:
+            line_item_lst = list()
+            split_line = line.split(' ')
+            for item in split_line:
+                if item not in ['', '\n']:
+                    line_item_lst.append(item)
+            #print(line_item_lst)
+            measured_point_lst.append(line_item_lst)
+        #for item in measured_point_lst:
+         #   print(item)
+        #print(measured_point_lst)
+
+    #photo_constant_params
+    #focal lenght
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_calibrated_internal_camera_parameters.cam'):
+                with open(f'{params_pix_path}{file}', 'r', encoding='utf-8') as cam_f:
+                    for line in cam_f:
+                        if line.startswith('FOCAL '):
+                            focal_len = line.split(' ')[1]
+                            focal_len = focal_len[:-2]
+
+    #write photos params
+    for photo in photo_lst:
+        #params for individual photo
+        #eo
+        for subdir, dirs, files in os.walk(params_pix_path):
+            for file in files:
+                if file.endswith('_calibrated_external_camera_parameters_error.txt'):
+                    df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+
+        for index, rows in df.iterrows():
+            if photo == rows.imageName:
+                photo_x = rows.X_opt
+                photo_y = rows.Y_opt
+                photo_z = rows.Z_opt
+        #measured control points
+
+
+        f.write(f'$PHOTO\n')
+        f.write(f'  $PHOTO_NUM : {photo[:-4]}\n')
+        f.write(f'  $PHOTO_FILE : {photos_path}{photo}\n')
+        f.write(f'  $CAMERA_ID : \n')
+        f.write(f'  $TERRAIN_HEIGHT : \n')
+        f.write(f'  $IO_PARS : \n')
+        f.write(f'  $EXT_ORI : \n')
+        f.write(f'\t\t{focal_len}\t{photo_x}\t{photo_y}\t{photo_z}\n')
+
+        f.write(f'  $PHOTO_POINTS :\n')
+        #for point in points_list:
+        #    print(f'point {point}')
+        for item in measured_point_lst:
+            if photo[:-4] == item[0]:
+                for point in points_list:
+                    if point == item[1][1:]:
+                        f.write(f'    {point}\t{item[3]}\t{item[2]}\t1.00\t2')
+                        f.write(' { * }\n')
+
+        f.write(f'  $END_POINTS\n')
+
+        f.write(f'$END\n')
+    #nav_gps_lst = list()
+    #for index, rows in df.iterrows():
+    #    gps_lst = [rows.imageName[:-4], rows.X_gps, rows.Y_gps, rows.Z_gps]
+    #    nav_gps_lst.append(gps_lst)
+
+
+    
 
     #CAMERA
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_calibrated_internal_camera_parameters.cam'):
+                with open(f'{params_pix_path}{file}', 'r', encoding='utf-8') as cam_f:
+                    for line in cam_f:
+                        if line.startswith('#Image size'):
+                            colums_rows_size = line.split(' ')[2]
+                            ccd_col = colums_rows_size.split('x')[0]
+                            ccd_rows = colums_rows_size.split('x')[1]
+                        elif line.startswith('FOCAL '):
+                            focal_len = line.split(' ')[1]
+                            focal_len = focal_len[:-2]
+                        elif '#Principal Point Offset xpoff ypoff in mm (Inpho)' in line:
+                            prin_point_PPA_lst = list()
+                            prin_point_PPA_lst.append(cam_f.__next__())
+                            prin_point_PPA_lst.append(cam_f.__next__())
+                            prin_point_PPA_x = prin_point_PPA_lst[0].split(' ')[1]
+                            prin_point_PPA_y = prin_point_PPA_lst[1].split(' ')[1]
+
+
+
+    #camera_type = 'hasselbladt'
+    #camera_date = '14:16:07 25/04/2017'
+    #camera_brand = 'Custom'
+    #camera_kind = 'CCDFrame'
+    #ccd_in_orient = ['-0.0000000000', '-166.6666666667', '4102.1667000000', '-166.6666666667', '-0.0000000000', '3052.7167000000']
+    #pxl_ref = 'CenterTopLeft'
+    #prin_point_PPA = ['0.000000', '0.000000']
+    #gps_antenna_offset = ['0.000000', '0.000000', '0.000000']
+    #camera_mount_rotation = '0.000000'
+
     f.write(f'$CAMERA\n')
-    f.write(f'  $TYPE : {camera_type}\n')
-    f.write(f'  $DATE : {camera_date}\n')
-    f.write(f'  $BRAND : {camera_brand}\n')
-    f.write(f'  $KIND : {camera_kind}\n')
+    f.write(f'  $TYPE : \n')
+    f.write(f'  $DATE : \n')
+    f.write(f'  $BRAND : \n')
+    f.write(f'  $KIND : \n')
     f.write(f'  $CCD_INTERIOR_ORIENTATION :\n')
-    f.write(f'    {ccd_in_orient[0]} {ccd_in_orient[1]} {ccd_in_orient[2]}\n')
-    f.write(f'    {ccd_in_orient[3]} {ccd_in_orient[4]} {ccd_in_orient[5]}\n')
+    f.write(f'    \n')
+    f.write(f'    \n')
     f.write(f'  $CCD_COLUMNS : {ccd_col}\n')
     f.write(f'  $CCD_ROWS : {ccd_rows}\n')
-    f.write(f'  $PIXEL_REFERENCE : {pxl_ref}\n')
+    f.write(f'  $PIXEL_REFERENCE : \n')
     f.write(f'  $FOCAL_LENGTH :    {focal_len}\n')
-    f.write(f'  $PRINCIPAL_POINT_PPA :     {prin_point_PPA[0]} {prin_point_PPA[0]}\n')
-    f.write(f'  $GPS_ANTENNA_OFFSET :     {gps_antenna_offset[0]} {gps_antenna_offset[1]} {gps_antenna_offset[2]}\n')
-    f.write(f'  $CAMERA_MOUNT_ROTATION :     {camera_mount_rotation}\n')
+    f.write(f'  $PRINCIPAL_POINT_PPA :     {prin_point_PPA_x[:-2]} {prin_point_PPA_y[:-2]}\n')
+    f.write(f'  $GPS_ANTENNA_OFFSET :     \n')
+    f.write(f'  $CAMERA_MOUNT_ROTATION :     \n')
     f.write(f'$END\n')
 
     #POINTS
-    f.write(f'$ADJUSTED_POINTS\n')
+    f.write(f'$CONTROL_POINTS\n')
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_measured_estimated_gcps_position.txt'):
+                df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+
+    points_lst = list()
+    for index, rows in df.iterrows():
+        f.write(f'  {rows["GCP label"]}\t{rows["GCP measured X"]}\t{rows["GCP measured Y"]}\t{rows["GCP measured Z"]}\t3\t0\t0\t0\n')
+
     f.write(f'$END_POINTS\n')
 
     #NAVIGATION
     f.write(f'$NAVIGATION\n')
-    f.write(f'{navigation}\n')
+    #f.write(f'{navigation}\n')
+
+    for subdir, dirs, files in os.walk(params_pix_path):
+        for file in files:
+            if file.endswith('_calibrated_external_camera_parameters_error.txt'):
+                df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+
+    nav_gps_lst = list()
+    for index, rows in df.iterrows():
+        gps_lst = [rows.imageName[:-4], rows.X_gps, rows.Y_gps, rows.Z_gps]
+        nav_gps_lst.append(gps_lst)
+    #print(nav_gps_lst)
+
+    for item in nav_gps_lst:
+        f.write(f'  $PHOTO_NUM : {item[0]}\n'
+                f'    $GPS :     {item[1]}\t{item[2]}\t{item[3]}\n'
+                f'    $INS :     \n')
+
     f.write(f'$ENDNAV\n')
+
     f.write(f"""$DESCRIPTIONS
       0 Undefined
       1 Signalized\ Point
--- inpho_pix_methods.py
+++ inpho_pix_methods.py
@@ -5,19 +5,35 @@ import os
 import argparse
 import pandas as pd
 
-params_pix_path = 'D:\\PW\\inpho_pix\\pix\\ok\\1_initial\\params\\'
+params_pix_path = 'D:\\PW\\inpho_pix\\Malbork_AT\\pix\\Malbork_pix\\1_initial\\params\\'
 
-for subdir, dirs, files in os.walk(params_pix_path):
-    for file in files:
-        if file.endswith('_calibrated_external_camera_parameters_error.txt'):
-            df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
 
-nav_gps_lst = list()
-for index, rows in df.iterrows():
-    gps_lst = [rows.imageName[:-4], rows.X_gps, rows.Y_gps, rows.Z_gps]
-    nav_gps_lst.append(gps_lst)
-print(nav_gps_lst)
+#navigation
+#def search_navigation_params(params_pix_path):
+#    for subdir, dirs, files in os.walk(params_pix_path):
+#        for file in files:
+#            if file.endswith('_calibrated_external_camera_parameters_error.txt'):
+#                df = pd.read_csv(f'{params_pix_path}{file}', delimiter=', ')
+#
+#    nav_gps_lst = list()
+#    for index, rows in df.iterrows():
+#        gps_lst = [rows.imageName[:-4], rows.X_gps, rows.Y_gps, rows.Z_gps]
+#        nav_gps_lst.append(gps_lst)
+#    print(nav_gps_lst)
+#
+#    nav_text = str()
+#
+#    for item in nav_gps_lst:
+#        return (f'  $PHOTO_NUM : {item[0]}\n'
+#              f'    $GPS :     {item[1]}\t{item[2]}\t{item[3]}')
 
-for item in nav_gps_lst:
-    print(f'  $PHOTO_NUM : {item[0]}\n'
-          f'    $GPS :     {item[1]}\t{item[2]}\t{item[3]}')
+#photo
+
+#import coord system
+def search_coor_system(params_pix_path):
+    for subdir, dirs, files in os.walk(params_pix_path):
+       for file in files:
+           if file.endswith('_wkt.prj'):
+               with open(f'{params_pix_path}{file}', 'r') as f:
+                   for line in f:
+                       return line
